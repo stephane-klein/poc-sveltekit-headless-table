65fc854fd30b88d6284b225383ad3ca1f581bcfa
Stéphane Klein <contact@stephane-klein.info>
2023-12-26 23:00:53 +0100

    Fix Popover zindex UI issue #2

--- src/lib/AddColumnHeaderCell.svelte
+++ src/lib/AddColumnHeaderCell.svelte
@@ -1,9 +1,23 @@
 <script>
+    import { onMount } from "svelte";
+    import { Popover, PopoverButton, PopoverPanel } from "@rgossiaux/svelte-headlessui";
+    import { createPopperActions } from "svelte-popperjs";
+
     export let state;
     export let columns;
     export let onChange;
 
-    import { Popover, PopoverButton, PopoverPanel } from "@rgossiaux/svelte-headlessui";
+    const [popperRef, popperContent] = createPopperActions({
+        placement: "bottom"
+    });
+    const extraOpts = {
+        modifiers: [{ name: "offset", options: { offset: [0, 8] } }]
+    };
+
+    let panel;
+    onMount(() => {
+        document.getElementsByTagName("body")[0].append(panel);
+    });
 </script>
 
 <Popover style="position: relative;">
@@ -19,6 +33,7 @@
             fill="none"
             stroke-linecap="round"
             stroke-linejoin="round"
+            use:popperRef
         >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M12 5l0 14" />
@@ -26,67 +41,70 @@
         </svg>
     </PopoverButton>
 
-    <PopoverPanel class="absolute -left-5 z-10 flex w-screen max-w-min px-4">
-        <div
-            class="w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow ring-1 ring-gray-900/5"
-        >
-            <h3 class="font-normal text-gray-900">Visible fields</h3>
+    <div bind:this={panel}>
+        <PopoverPanel class="absolute -left-5 z-10 flex w-screen max-w-min px-4">
+            <div
+                class="w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow ring-1 ring-gray-900/5"
+                use:popperContent={extraOpts}
+            >
+                <h3 class="font-normal text-gray-900">Visible fields</h3>
 
-            <div class="flex flex-col gap-1 my-2">
-                {#each Object.entries(columns) as [fieldId, fieldCaption]}
-                    <div
-                        class={`cursor-pointer hover:bg-gray-100 px-2 py-1 rounded-md ${
-                            state[fieldId] === true ? "" : "hidden"
-                        }`}
-                    >
-                        <input
-                            id={`add_column_header_cell_visible_field_${fieldId}`}
-                            class="cursor-pointer"
-                            type="checkbox"
-                            bind:checked={state[fieldId]}
-                            on:change={() => {
-                                onChange(state);
-                            }}
-                        />
-                        <label
-                            for={`add_column_header_cell_visible_field_${fieldId}`}
-                            class="text-base cursor-pointer font-normal text-gray-900"
+                <div class="flex flex-col gap-1 my-2">
+                    {#each Object.entries(columns) as [fieldId, fieldCaption]}
+                        <div
+                            class={`cursor-pointer hover:bg-gray-100 px-2 py-1 rounded-md ${
+                                state[fieldId] === true ? "" : "hidden"
+                            }`}
                         >
-                            &nbsp;{fieldCaption}
-                        </label>
-                    </div>
-                {/each}
-            </div>
+                            <input
+                                id={`add_column_header_cell_visible_field_${fieldId}`}
+                                class="cursor-pointer"
+                                type="checkbox"
+                                bind:checked={state[fieldId]}
+                                on:change={() => {
+                                    onChange(state);
+                                }}
+                            />
+                            <label
+                                for={`add_column_header_cell_visible_field_${fieldId}`}
+                                class="text-base cursor-pointer font-normal text-gray-900"
+                            >
+                                &nbsp;{fieldCaption}
+                            </label>
+                        </div>
+                    {/each}
+                </div>
 
-            <h3 class="font-normal text-gray-900">Hidden fields</h3>
+                <h3 class="font-normal text-gray-900">Hidden fields</h3>
 
-            <div class="flex flex-col gap-1 my-2">
-                {#each Object.entries(columns) as [fieldId, fieldCaption]}
-                    <div
-                        class={`cursor-pointer hover:bg-gray-100 px-2 py-1 rounded-md ${
-                            state[fieldId] === false ? "" : "hidden"
-                        }`}
-                    >
-                        <input
-                            id={`add_column_header_cell_hide_field_${fieldId}`}
-                            type="checkbox"
-                            bind:checked={state[fieldId]}
-                            on:change={(event) => {
-                                console.log("event.target.checked", event.target.checked);
-                                console.log(`state[${fieldId}]`, state[fieldId]);
-                                console.log(`state[${fieldId}]`, true);
-                                onChange(state);
-                            }}
-                        />
-                        <label
-                            for={`add_column_header_cell_hide_field_${fieldId}`}
-                            class="text-base cursor-pointer font-normal text-gray-900"
+                <div class="flex flex-col gap-1 my-2">
+                    {#each Object.entries(columns) as [fieldId, fieldCaption]}
+                        <div
+                            class={`cursor-pointer hover:bg-gray-100 px-2 py-1 rounded-md ${
+                                state[fieldId] === false ? "" : "hidden"
+                            }`}
                         >
-                            &nbsp;{fieldCaption}
-                        </label>
-                    </div>
-                {/each}
+                            <input
+                                id={`add_column_header_cell_hide_field_${fieldId}`}
+                                type="checkbox"
+                                bind:checked={state[fieldId]}
+                                on:change={(event) => {
+                                    console.log("event.target.checked", event.target.checked);
+                                    console.log(`state[${fieldId}]`, state[fieldId]);
+                                    console.log(`state[${fieldId}]`, true);
+                                    onChange(state);
+                                }}
+                            />
+                            <label
+                                for={`add_column_header_cell_hide_field_${fieldId}`}
+                                class="text-base cursor-pointer font-normal text-gray-900"
+                            >
+                                &nbsp;{fieldCaption}
+                            </label>
+                        </div>
+                    {/each}
+                </div>
             </div>
-        </div>
-    </PopoverPanel>
+        </PopoverPanel>
+    </div>
 </Popover>
--- src/lib/DefaultHeaderCell.svelte
+++ src/lib/DefaultHeaderCell.svelte
@@ -1,8 +1,22 @@
 <script>
+    import { onMount } from "svelte";
     import { Popover, PopoverButton, PopoverPanel } from "@rgossiaux/svelte-headlessui";
+    import { createPopperActions } from "svelte-popperjs";
 
     export let title;
     export let onHideField;
+
+    const [popperRef, popperContent] = createPopperActions({
+        placement: "bottom"
+    });
+    const extraOpts = {
+        modifiers: [{ name: "offset", options: { offset: [0, 8] } }]
+    };
+
+    let panel;
+    onMount(() => {
+        document.getElementsByTagName("body")[0].append(panel);
+    });
 </script>
 
 <div class="flex flex-row gap-2 items-center">
@@ -22,6 +36,7 @@
                 fill="none"
                 stroke-linecap="round"
                 stroke-linejoin="round"
+                use:popperRef
             >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
@@ -30,17 +45,20 @@
             </svg>
         </PopoverButton>
 
-        <PopoverPanel class="absolute -left-5 z-100 flex w-screen max-w-min px-4">
-            <div
-                class="w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow ring-1 ring-gray-900/5"
-            >
-                <a
-                    on:click={onHideField}
-                    class="cursor-pointer hover:bg-gray-100 w-full block px-2 py-1 rounded-md text-gray-500"
+        <div bind:this={panel}>
+            <PopoverPanel class="absolute -left-5 z-100 flex w-screen max-w-min px-4">
+                <div
+                    class="w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow ring-1 ring-gray-900/5"
+                    use:popperContent={extraOpts}
                 >
-                    Hide field
-                </a>
-            </div>
-        </PopoverPanel>
+                    <a
+                        on:click={onHideField}
+                        class="cursor-pointer hover:bg-gray-100 w-full block px-2 py-1 rounded-md text-gray-500"
+                    >
+                        Hide field
+                    </a>
+                </div>
+            </PopoverPanel>
+        </div>
     </Popover>
 </div>
